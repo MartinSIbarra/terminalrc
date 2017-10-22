7b38aa6873efcd02bf403cf3802c52046f96ea08
Pablo Scolpino <martinibarra@gmail.com>
2017-10-22 19:37:46 -0300

    color added to display list aliases 'a' and 'l'

--- .config/alias.sh
+++ .config/alias.sh
@@ -1,4 +1,4 @@
-alias l='ls -l -G'
-alias a='clear;ls -l -G'
+alias l='ls -l -G --color'
+alias a='clear;ls -l -G --color'
 alias ap='ansible-playbook'
 git config --global alias.up 'pull --rebase --autostash'
