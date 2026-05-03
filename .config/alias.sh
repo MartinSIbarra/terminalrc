691c094d35bce5d03914f2db44960c9346a173ce
Martín Ibarra <martinibarra@gmail.com>
2026-05-02 23:56:20 -0300

    update ls aliases to include group-directories-first option

--- .config/alias.sh
+++ .config/alias.sh
@@ -1,5 +1,5 @@
-alias l='ls -l -G --color'
-alias a='ls -la -G --color'
+alias l='ls -l -G --color --group-directories-first'
+alias a='ls -la -G --color --group-directories-first'
 alias jenkins-up="docker compose -f $HOME/workspace/jenkins/docker-compose-jenkins/docker-compose.yml up -d"
 alias jenkins-down="docker compose -f $HOME/workspace/jenkins/docker-compose-jenkins/docker-compose.yml down"
 git config --global alias.up 'pull --rebase --autostash'
