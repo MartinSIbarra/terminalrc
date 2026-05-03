5c620ebf072350ac1d4285a6bb600ed860c6b91c
Martín Ibarra <martinibarra@gmail.com>
2026-05-03 18:52:55 -0300

    add bash completion support to source_this.sh

--- .config/source_this.sh
+++ .config/source_this.sh
@@ -9,3 +9,11 @@ CONFIG=$HOME/.config
 [ -f $CONFIG/extras.sh ] && source $CONFIG/extras.sh
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
